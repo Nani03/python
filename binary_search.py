aacade701c931d511c519e11d9ceba7230602f90
K Leela Krishna <leela.rroyal@gmail.com>
2019-09-04 18:12:10 +0530

    Add files via upload

---
+++ binary_search.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[215]:
+
+
+import random
+def linear_search():
+    l=[]
+    for x in range(5):
+        l.append(random.randint(1,10))
+    print(l)
+    target = int(random.randint(1,10))
+    print(a)
+    for i in range(len(l)):
+        if l[i] == target:
+            print("true")
+        else:
+            print("false")
+
+
+# In[216]:
+
+
+linear_search()
+
+
+# In[ ]:
+
+
+
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
